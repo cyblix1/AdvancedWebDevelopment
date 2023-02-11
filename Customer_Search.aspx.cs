40ba8630eaa2fa86cb817e845775fe08cf36f1c4
nathanael <nattzwc@gmail.com>
2023-02-11 11:31:27 +0800

    .

--- Customer_Search.aspx.cs
+++ Customer_Search.aspx.cs
@@ -14,41 +14,43 @@ namespace AdvancedWebDevelopment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            txtSearch.Text = Session["Search"].ToString();
-
-            string strConnectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
-            SqlConnection myConnect = new SqlConnection(strConnectionString);
-            myConnect.Open();
-            string checksearch = "SELECT COUNT(*) FROM [Products] WHERE name LIKE @search or category LIKE @search";
-            SqlCommand com = new SqlCommand(checksearch, myConnect);
-            com.Parameters.AddWithValue("@search", txtSearch.Text);
-            com.Parameters["@search"].Value = "%" + txtSearch.Text + "%";
-            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
-            myConnect.Close();
-            if (temp > 0)
+            if (!IsPostBack)
             {
-                string strCommandText = "SELECT *";
-                strCommandText += " FROM Products WHERE name LIKE @name OR Category LIKE @category";
-                SqlCommand cmd = new SqlCommand(strCommandText, myConnect);
-                cmd.Parameters.Add("@name", SqlDbType.NVarChar, 50);
-                cmd.Parameters["@name"].Value = "%" + txtSearch.Text + "%";
-                cmd.Parameters.Add("@category", SqlDbType.NVarChar, 50);
-                cmd.Parameters["@category"].Value = "%" + txtSearch.Text + "%";
+                txtSearch.Text = Session["Search"].ToString();
+
+                string strConnectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+                SqlConnection myConnect = new SqlConnection(strConnectionString);
                 myConnect.Open();
-                cmd.ExecuteNonQuery();
-                SqlDataAdapter da = new SqlDataAdapter();
-                da.SelectCommand = cmd;
-                DataSet ds = new DataSet();
-                da.Fill(ds, "Title");
-                Repeater1.DataSource = ds;
-                Repeater1.DataBind();
+                string checksearch = "SELECT COUNT(*) FROM [Products] WHERE name LIKE @search or category LIKE @search";
+                SqlCommand com = new SqlCommand(checksearch, myConnect);
+                com.Parameters.AddWithValue("@search", txtSearch.Text);
+                com.Parameters["@search"].Value = "%" + txtSearch.Text + "%";
+                int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
                 myConnect.Close();
+                if (temp > 0)
+                {
+                    string strCommandText = "SELECT *";
+                    strCommandText += " FROM Products WHERE name LIKE @name OR Category LIKE @category";
+                    SqlCommand cmd = new SqlCommand(strCommandText, myConnect);
+                    cmd.Parameters.Add("@name", SqlDbType.NVarChar, 50);
+                    cmd.Parameters["@name"].Value = "%" + txtSearch.Text + "%";
+                    cmd.Parameters.Add("@category", SqlDbType.NVarChar, 50);
+                    cmd.Parameters["@category"].Value = "%" + txtSearch.Text + "%";
+                    myConnect.Open();
+                    cmd.ExecuteNonQuery();
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "Title");
+                    Repeater1.DataSource = ds;
+                    Repeater1.DataBind();
+                    myConnect.Close();
+                }
+                else
+                {
+                    lblnoresult.Visible = true;
+                }
             }
-            else
-            {
-                lblnoresult.Visible = true;
-            }
-
         }
 
         protected void btnSearch_Click(object sender, EventArgs e)
